c3e164a310823f9abe78c61169dbc6f8a59fc5f8
RonaldoSilva <ronjsilva@gmail.com>
2025-11-02 09:25:06 -0300

    atualizacao 02 11 2025  senha

--- build.sh
+++ build.sh
@@ -5,4 +5,4 @@
 pip install -r requirements.txt
 python manage.py collectstatic --noinput
 python manage.py migrate
-
+python create_admin.py
--- core/settings.py
+++ core/settings.py
@@ -87,22 +87,6 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # Database
 DATABASE_URL = config('DATABASE_URL', default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}")
 
-if DATABASE_URL.startswith('sqlite'):
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
-    }
-else:
-    DATABASES = {
-        'default': dj_database_url.config(
-            default=DATABASE_URL,
-            conn_max_age=600,
-            ssl_require=True
-        )
-    }
-
 # Password validation
 AUTH_PASSWORD_VALIDATORS = [
     {
